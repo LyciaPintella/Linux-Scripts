c948de4bfd54aaf6a90962b7ebde1fddba0ae43e
Jessica Murphy <Jessica.Ariadne.Murphy@GMail.com>
2026-04-18 00:09:49 -0400

    Hi

---
+++ Symbolic-Links.sh
@@ -0,0 +1,4 @@
+# ln -s <target> <link_name>
+
+ln -s "/home/jessica/Google Drive/Linux Scripts" "/home/jessica/Desktop/Linux Scripts"
+ln -s "/home/jessica/Google Drive" "/home/jessica/Desktop/Google Drive"
